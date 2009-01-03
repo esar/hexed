03e1f2e25e10270edf08285481274e5d07047917
esar <hacks@esar.org.uk>
2009-01-03 16:34:46 +0000

    Added MD5, SHA256, SHA384, SHA512 and RIPEMD160 to checksum plugin

--- ChecksumPlugin/Checksum.cs
+++ ChecksumPlugin/Checksum.cs
@@ -7,12 +7,24 @@ using System.Text;
 
 namespace ChecksumPlugin
 {
+	public class ChecksumResult
+	{
+		public string Name;
+		public byte[] Checksum;
+		
+		public ChecksumResult(string name, byte[] checksum)
+		{
+			Name = name;
+			Checksum = checksum;
+		}
+	}
+	
 	public class ResultWindow : Form
 	{
 		IPluginHost Host;
 		BackgroundWorker Worker;
 
-		Panel ControlPanel = new Panel();
+		TableLayoutPanel ControlPanel = new TableLayoutPanel();
 		Button CalcButton = new Button();
 		
 		ListView ResultList = new ListView();
@@ -20,6 +32,13 @@ namespace ChecksumPlugin
 		StatusStrip StatusBar = new StatusStrip();
 		ToolStripProgressBar ProgressBar = new ToolStripProgressBar();
 		ToolStripLabel ProgressLabel = new ToolStripLabel();
+
+		string[] Algorithms = { "MD5",
+			                    "SHA1",
+			                    "SHA256",
+			                    "SHA384",
+			                    "SHA512",
+			                    "RIPEMD160" };
 		
 		public ResultWindow(IPluginHost host)
 		{
@@ -31,19 +50,31 @@ namespace ChecksumPlugin
 			Worker.DoWork += OnDoWork;
 			Worker.ProgressChanged += OnProgressChanged;
 			Worker.RunWorkerCompleted += OnCompleted;
-			
+
 			ResultList.View = View.Details;
+			ResultList.CheckBoxes = true;
 			ResultList.Columns.Add("Algorithm");
 			ResultList.Columns.Add("Result");
+			foreach(string algo in Algorithms)
+			{
+				ListViewItem item = ResultList.Items.Add(algo);
+				item.Name = algo;
+				item.Checked = true;
+				item.SubItems.Add(String.Empty);
+			}
 			ResultList.Dock = DockStyle.Fill;
 			Controls.Add(ResultList);
 			
 			CalcButton.Click += OnCalculate;
 			CalcButton.Text = "Calculate";
 			CalcButton.AutoSize = true;
-			CalcButton.Dock = DockStyle.Right;
+			CalcButton.Anchor = AnchorStyles.Right;
 			ControlPanel.AutoSize = true;
-			ControlPanel.Controls.Add(CalcButton);
+			ControlPanel.RowCount = 1;
+			ControlPanel.ColumnCount = 2;
+			ControlPanel.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100));
+			ControlPanel.ColumnStyles.Add(new ColumnStyle(SizeType.AutoSize));
+			ControlPanel.Controls.Add(CalcButton, 1, 0);
 			ControlPanel.Dock = DockStyle.Top;
 			Controls.Add(ControlPanel);
 			
@@ -61,7 +92,9 @@ namespace ChecksumPlugin
 		
 		public void OnCalculate(object sender, EventArgs e)
 		{
-			ResultList.Clear();
+			foreach(ListViewItem i in ResultList.Items)
+				if(i.SubItems.Count > 1)
+					i.SubItems[1].Text = String.Empty;
 			
 			if(Worker.IsBusy)
 			{
@@ -70,18 +103,27 @@ namespace ChecksumPlugin
 				return;
 			}
 			
+			List<string> enabledAlgorithms = new List<string>();
+			foreach(ListViewItem i in ResultList.Items)
+				if(i.Checked)
+					enabledAlgorithms.Add(i.Name);
+			
 			CalcButton.Text = "Cancel";
 			ProgressLabel.Text = "Calculating...";
-			Worker.RunWorkerAsync();
+			Worker.RunWorkerAsync(enabledAlgorithms);
 		}
 		
 		public void OnDoWork(object sender, DoWorkEventArgs e)
 		{
 			const int BLOCK_SIZE = 4096*1024;
 			
-			System.Security.Cryptography.MD5 md5 = System.Security.Cryptography.MD5.Create();
-			System.Security.Cryptography.SHA1 sha1 = System.Security.Cryptography.SHA1.Create();
-			sha1.Initialize();
+			Dictionary<string, System.Security.Cryptography.HashAlgorithm> algos = new Dictionary<string, System.Security.Cryptography.HashAlgorithm>();
+			foreach(string algo in (List<string>)e.Argument)
+			{
+				System.Security.Cryptography.HashAlgorithm ha = System.Security.Cryptography.CryptoConfig.CreateFromName(algo) as System.Security.Cryptography.HashAlgorithm;
+				ha.Initialize();
+				algos.Add(algo, ha);
+			}
 
 			// TODO: Need thread safe method of accessing the buffer
 			// TODO: Need to know if the buffer changes while we're processing
@@ -97,9 +139,8 @@ namespace ChecksumPlugin
 				int partLen = BLOCK_SIZE > len ? (int)len : BLOCK_SIZE;
 				buffer.GetBytes(offset, bytes, partLen);
 
-				md5.TransformBlock(bytes, 0, partLen, bytes, 0);
-				sha1.TransformBlock(bytes, 0, partLen, bytes, 0);
-				
+				foreach(KeyValuePair<string, System.Security.Cryptography.HashAlgorithm> ha in algos)
+					ha.Value.TransformBlock(bytes, 0, partLen, bytes, 0);
 				
 				len -= partLen;
 				offset += partLen;
@@ -116,10 +157,14 @@ namespace ChecksumPlugin
 					return;
 				}
 			}
+
+			foreach(KeyValuePair<string, System.Security.Cryptography.HashAlgorithm> ha in algos)
+				ha.Value.TransformFinalBlock(bytes, 0, 0);
 			
-			md5.TransformFinalBlock(bytes, 0, 0);
-			sha1.TransformFinalBlock(bytes, 0, 0);
-			e.Result = sha1.Hash.Clone();
+			List<ChecksumResult> results = new List<ChecksumResult>();
+			foreach(KeyValuePair<string, System.Security.Cryptography.HashAlgorithm> ha in algos)
+				results.Add(new ChecksumResult(ha.Key, ha.Value.Hash));
+			e.Result = results;
 		}
 		
 		public void OnProgressChanged(object sender, ProgressChangedEventArgs e)
@@ -132,11 +177,17 @@ namespace ChecksumPlugin
 		{
 			if(e.Cancelled == false)
 			{
-				StringBuilder hash = new StringBuilder();
-				foreach(byte b in (byte[])e.Result)
-					hash.Append(b.ToString("X2"));
-				ListViewItem item = ResultList.Items.Add("SHA1");
-				item.SubItems.Add(hash.ToString());
+				List<ChecksumResult> results = e.Result as List<ChecksumResult>;
+				
+				foreach(ChecksumResult result in results)
+				{
+					StringBuilder hash = new StringBuilder();
+					foreach(byte b in result.Checksum)
+						hash.Append(b.ToString("X2"));
+					ListViewItem[] items = ResultList.Items.Find(result.Name, false);
+					if(items.Length == 1)
+						items[0].SubItems[1].Text = hash.ToString();
+				}
 			}
 			
 			ProgressLabel.Text = "Ready";
