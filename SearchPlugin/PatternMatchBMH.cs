f1f4b9a1409b99414c4c35a24cd521597cb3c6f1
esar <hacks@esar.org.uk>
2009-01-13 22:32:01 +0000

    Added SearchPlugin

---
+++ SearchPlugin/PatternMatchBMH.cs
@@ -0,0 +1,143 @@
+using System;
+using System.Collections.Generic;
+
+class PatternMatchBMH
+{
+	int[] Jump = new int[0x100];
+	byte[] Pattern;
+	byte[] PartialText;
+	int PartialTextSize;
+	
+	public PatternMatchBMH()
+	{
+	}
+	
+	public void Initialize(string pattern, bool caseSensitive)
+	{
+		if(caseSensitive)
+			Pattern = System.Text.ASCIIEncoding.UTF8.GetBytes(pattern);
+		else
+			Pattern = System.Text.ASCIIEncoding.UTF8.GetBytes(pattern.ToLower());
+
+		PartialText = new byte[Pattern.Length];
+		PartialTextSize = 0;
+		
+		int i;
+		for(i = 0; i < 0x100; ++i)
+			Jump[i] = Pattern.Length;
+
+		for(i = pattern.Length - 1; i >= 0; --i)
+		{
+			byte b = Pattern[i];
+			int shift = Pattern.Length - i - 1;
+			if(shift > 0 && shift < Jump[b])
+				Jump[b] = shift;
+		}
+	}
+	
+	private int CompareByteArrays(byte[] array1, int offset1, byte[] array2, int offset2, int length)
+	{
+		for(int i = 0; i < length; ++i)
+		{
+			int result = array1[offset1 + i] - array2[offset2 + i];
+			if(result != 0)
+				return result;
+		}
+		
+		return 0;
+	}
+	
+	public IEnumerable<int> SearchBlock(byte[] block, int offset, int count)
+	{
+		int i = offset;
+		int len = Pattern.Length;
+		int end = offset + count;
+
+		// If we're continuing a search that started in a previous block, we'll have
+		// a few chars left over which might be part of the string we're searching for.
+		// So we need to do the comparissons in two parts until we're working entirely
+		// in the new block, then we can continue as normal.
+		if(PartialTextSize > 0)
+		{
+			// Wind the pointer back to where it would be if all the data was in this packet
+			// It's ok to do this as we're only looking at the last byte which is definately 
+			// in this new packet
+			i -= PartialTextSize - 1;
+
+			// This while loop breaks when we get to the end of the block _or_ when we're
+			// working entirely in the new block and can use the main loop below
+			while((i + len) < end && i < offset)
+			{
+				// Get the last character and convert it to lower case
+				byte b = block[i + len - 1];
+				// TODO: Case insensitive
+				//if(c <= 'Z' && c >= 'A')
+				//	c |= 32;
+				
+				// Comparison has to be done in two parts, first part in state->partialText
+				// second part in the packet
+				
+				if(b == Pattern[len - 1] && 
+				   CompareByteArrays(PartialText, PartialTextSize - (offset - i), Pattern, 0, offset - i) == 0  &&
+				   CompareByteArrays(block, offset - i, Pattern, offset - i, len - (offset - i)) == 0)
+				{
+					i += len;
+					offset = i;
+					// Found pattern
+					yield return offset;
+				}
+				else
+				{
+					// If no match, skip ahead x chars, as specified by the bad char jump table
+					i += Jump[b];
+				}
+			}
+
+			PartialTextSize = 0;
+		}
+
+		// Main boyer-moore-horspool string search loop
+		while((i + len) < end)
+		{
+			// Get last character of potential match and convert to lower case
+			byte b = block[i + len - 1];
+			// TODO: Case insensitive
+			//if(c <= 'Z' && c >= 'A')
+			//	c |= 32;
+
+			// If it matches, check the whole string for a match
+			if(b == Pattern[len - 1] && CompareByteArrays(block, i, Pattern, 0, len) == 0)
+			{
+				i += len;
+				offset = i;
+				// Found pattern
+				yield return offset;
+			}
+			else
+			{
+				// If no match, skip ahead x chars, as specified by the bad char jump table
+				i += Jump[b];
+			}
+		}
+
+		offset = i;
+		
+		// finished searching in this packet, but do we need those last few chars next time?
+		if(end - offset > 0)
+		{
+			if(PartialText.Length < PartialTextSize + (end - offset))
+			{
+				Console.WriteLine("BMH Partial Text Resize: " + (PartialTextSize + (end - offset)));
+				Array.Resize(ref PartialText, (PartialTextSize + (end - offset)));
+			}
+			
+			Array.Copy(block, offset, PartialText, PartialTextSize, end - offset);
+			PartialTextSize += end - offset;
+		}
+	}
+	
+	public IEnumerable<int> SearchFinalBlock(byte[] block, int offset, int count)
+	{
+		return SearchBlock(block, offset, count);
+	}
+}
---
+++ SearchPlugin/Search.cs
@@ -0,0 +1,208 @@
+using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+using System.Collections.Generic;
+
+namespace SearchPlugin
+{
+	class DocumentMatchBucketCollection
+	{
+		DocumentMatchIndicator Owner;
+		long DocumentLength;
+		bool[] Buckets;
+		public long BucketWidth;
+		
+		public int BucketCount
+		{
+			get { return Buckets.Length; }
+		}
+		
+		public bool this[int i]
+		{
+			get { return Buckets[i]; }
+		}
+		
+		public DocumentMatchBucketCollection(DocumentMatchIndicator owner, long documentLength, int numBuckets)
+		{
+			Owner = owner;
+			DocumentLength = documentLength;
+			Buckets = new bool[numBuckets];
+			BucketWidth = documentLength / numBuckets;
+		}
+		
+		public void Add(long position)
+		{
+			Buckets[position / BucketWidth] = true;
+			Owner.Invalidate();
+		}
+	}
+	
+	class DocumentMatchIndicator : UserControl
+	{
+		private long _SelectedMatch = -1;
+		public long SelectedMatch
+		{
+			get { return _SelectedMatch; }
+			set { _SelectedMatch = value; Invalidate(); }
+		}
+		
+		public DocumentMatchBucketCollection Matches;
+		
+		protected SolidBrush Brush;
+		protected SolidBrush HighlightBrush;
+		
+		public DocumentMatchIndicator()
+		{
+			Brush = new SolidBrush(Color.Blue);
+			HighlightBrush = new SolidBrush(Color.Red);
+			Width = 25;
+		}
+
+		public void Reset(long documentLength, int numBuckets)
+		{
+			Matches = new DocumentMatchBucketCollection(this, documentLength, numBuckets);
+		}
+		
+		protected override void OnPaint(PaintEventArgs e)
+		{
+			const int border = 5;
+			
+			base.OnPaint(e);
+
+			if(Matches == null)
+				return;
+			
+			float bucketPixelWidth = (float)(ClientSize.Height - border*2) / Matches.BucketCount;
+			
+			for(int i = 0; i < Matches.BucketCount; ++i)
+			{
+				if(Matches[i])
+				{
+					RectangleF r = new RectangleF(border, bucketPixelWidth * i + border, ClientSize.Width - border*2, bucketPixelWidth);
+
+					if(i != _SelectedMatch / Matches.BucketWidth)
+						e.Graphics.FillRectangle(Brush, r);
+				}
+			}
+
+			if(_SelectedMatch >= 0)
+			{
+				int i = (int)(_SelectedMatch / Matches.BucketWidth);
+				RectangleF r = new RectangleF(border, bucketPixelWidth * i + border, ClientSize.Width - border*2, bucketPixelWidth);
+				e.Graphics.FillRectangle(HighlightBrush, r);
+			}				
+			
+			e.Graphics.DrawRectangle(SystemPens.ButtonShadow, new Rectangle(border, border, ClientSize.Width - 2*border, ClientSize.Height - 2*border));
+		}
+	}
+	
+	class SearchPanel : Panel
+	{
+		IPluginHost Host;
+		DocumentMatchIndicator MatchIndicator = new DocumentMatchIndicator();
+		ListView ListView = new ListView();
+		ToolStrip ToolBar = new ToolStrip();
+		
+		public SearchPanel(IPluginHost host)
+		{
+			Host = host;
+			
+			ListView.Columns.Add("Position");
+			ListView.Columns.Add("Length");
+			ListView.Columns.Add("Value");
+			ListView.View = View.Details;
+			ListView.Dock = DockStyle.Fill;
+			Controls.Add(ListView);
+			
+			MatchIndicator.Dock = DockStyle.Left;
+			Controls.Add(MatchIndicator);
+			
+			ToolBar.GripStyle = ToolStripGripStyle.Hidden;
+			ToolStripItem item = new ToolStripButton(Host.Settings.Image("newfolder_16.png"));
+			item.ToolTipText = "New Search";
+			item.Click += OnSearch;
+			ToolBar.Items.Add(item);
+			ToolBar.Items.Add(new ToolStripSeparator());
+			ToolBar.Items.Add(Host.Settings.Image("first_16.png"));
+			ToolBar.Items.Add(Host.Settings.Image("prev_16.png"));
+			ToolBar.Items.Add(Host.Settings.Image("next_16.png"));
+			ToolBar.Items.Add(Host.Settings.Image("last_16.png"));			
+			Controls.Add(ToolBar);
+			
+			
+			ListView.DoubleClick += OnListViewDoubleClick;
+			ListView.SelectedIndexChanged += OnListViewSelectedIndexChanged;
+		}
+		
+		public IEnumerable<long> Search(Document document, string pattern)
+		{
+			PatternMatchBMH matcher = new PatternMatchBMH();
+			matcher.Initialize(pattern, true);
+
+			long offset = 0;
+			byte[] data = new byte[1024*1024];
+			while(offset < document.Length)
+			{
+				long len = (document.Length - offset) > (1024 * 1024) ? (1024 * 1024) : (document.Length - offset);
+				document.GetBytes(offset, data, len);
+				
+				foreach(int i in matcher.SearchBlock(data, 0, (int)len))
+					yield return offset + i;
+
+				offset += len;
+			}
+		}
+
+		protected void OnSearch(object sender, EventArgs e)
+		{
+			ListView.Items.Clear();
+			MatchIndicator.Reset(Host.ActiveView.Document.Length, 512);
+			
+			foreach(long i in Search(Host.ActiveView.Document, "Hex"))
+			{
+				MatchIndicator.Matches.Add(i);
+				ListViewItem item = ListView.Items.Add(i.ToString());
+				item.SubItems.Add("0");
+				item.SubItems.Add("Hex");
+			}
+		}
+		
+		protected void OnListViewDoubleClick(object sender, EventArgs e)
+		{
+			if(ListView.SelectedItems.Count > 0)
+			{
+				ListViewItem item = ListView.SelectedItems[0];
+				long start = Convert.ToInt64(item.Text) * 8;
+				long end = Convert.ToInt64(item.Text) * 8;
+				Host.ActiveView.Selection.Set(start, end);
+				Host.ActiveView.EnsureVisible(start);
+			}
+		}
+		
+		protected void OnListViewSelectedIndexChanged(object sender, EventArgs e)
+		{
+			if(ListView.SelectedItems.Count > 0)
+				MatchIndicator.SelectedMatch = Convert.ToInt64(ListView.SelectedItems[0].Text);
+			else
+				MatchIndicator.SelectedMatch = -1;
+		}
+	}
+	
+	
+	public class SearchPlugin : IPlugin
+	{
+		string IPlugin.Name { get { return "Search"; } }
+		string IPlugin.Author { get { return "Stephen Robinson"; } }
+		string IPlugin.Version { get { return "1.0"; } }
+
+		void IPlugin.Initialize(IPluginHost host)
+		{
+			host.AddWindow(new SearchPanel(host), "Search");
+		}
+		
+		void IPlugin.Dispose()
+		{
+		}
+	}
+}
