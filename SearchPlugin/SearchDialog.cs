7fb23879b30443d6a6c20b2faf308c2be628dcf1
esar <hacks@esar.org.uk>
2009-02-01 02:38:32 +0000

    Changed SearchDialog to be based on DialogBase

--- SearchPlugin/SearchDialog.cs
+++ SearchPlugin/SearchDialog.cs
@@ -4,12 +4,9 @@ using System.Windows.Forms;
 
 namespace SearchPlugin
 {
-	class SearchDialog : Form
+	class SearchDialog : DialogBase
 	{
 		TextBox		PatternTextBox = new TextBox();
-		Panel		ButtonPanel = new Panel();
-		Button		OkButton = new Button();
-		Button		CancelButton = new Button();
 
 		public string Pattern
 		{
@@ -18,31 +15,9 @@ namespace SearchPlugin
 		
 		public SearchDialog()
 		{
-			OkButton.Text = "OK";
-			OkButton.Dock = DockStyle.Right;
-			CancelButton.Text = "Cancel";
-			CancelButton.Dock = DockStyle.Right;
-			ButtonPanel.Controls.Add(CancelButton);
-			ButtonPanel.Controls.Add(OkButton);
-			ButtonPanel.Dock = DockStyle.Bottom;
-			Controls.Add(ButtonPanel);
+			Text = "Search";
 			PatternTextBox.Dock = DockStyle.Top;
 			Controls.Add(PatternTextBox);
-			
-			OkButton.Click += OnOK;
-			CancelButton.Click += OnCancel;
-		}
-		
-		protected void OnOK(object sender, EventArgs e)
-		{
-			DialogResult = DialogResult.OK;
-			Close();
-		}
-		
-		protected void OnCancel(object sender, EventArgs e)
-		{
-			DialogResult = DialogResult.Cancel;
-			Close();
-		}
+		}		
 	}
 }
