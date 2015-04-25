794627db241eead176eedd38915dc5114506404d
esar <hacks@esar.org.uk>
2015-04-25 20:14:32 +0100

    removed unrelated text from about dialog

--- AboutDialog.cs
+++ AboutDialog.cs
@@ -27,13 +27,7 @@ class AboutDialog : Form
 	{
 		Label label = new Label();
 
-		label.Text =	"Performance Viewer\n" +
-						"\n" +
-						"0.04  -  Fixed exception when counter goes away\n" +
-						"0.03  -  Added auto reduce to Y scale\n" +
-						"            Added reset toolbar button\n" +
-						"0.02  -  Fixed '% disk...' counters\n" +
-						"0.01  -  Initial release";
+		label.Text = "HexEd  v0.01";
 		label.Dock = DockStyle.Fill;
 
 		Controls.Add(label);
